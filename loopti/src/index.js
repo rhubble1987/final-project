ec44779c7c0c41ec1da3c18221fbf34d15cd00f8
munozjuan <munoz.juan07@hotmail.com>
2021-03-16 21:00:18 -0400

    routes for login

--- loopti/src/index.js
+++ loopti/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
