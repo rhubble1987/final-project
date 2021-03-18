0eabcd6a9805b5ab9f482335834d27604b3bc28d
Ryan Hubble <rhubble1987@gmail.com>
2021-03-18 08:36:19 -0400

    installed express and lint

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http').Server(app);
+const path = require('path');
+
+const PORT = process.env.PORT || 3005;
+
+const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+db.sequelize.sync().then(() => {
+    http.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+});
