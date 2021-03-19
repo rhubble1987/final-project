cf44f9f21b1da7bdaa5e317d8a60af85453ad6b3
munozjuan <munoz.juan07@hotmail.com>
2021-03-18 20:53:04 -0400

    added ecmaVersion: 2017 to eslintrc

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
+require('dotenv').config();
 
-module.exports = 
+module.exports =
 {
-  "development": {
-    "username": "root",
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  'development': {
+    'username': 'root',
+    'password': process.env.PASSWORD,
+    'database': process.env.DATABASE,
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_test',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
   },
   "production": {
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
-}
+};
