bfe11c101bcc282f431e34607bc87f214a829316
munozjuan <munoz.juan07@hotmail.com>
2021-03-20 13:20:27 -0400

    working on server.js password

--- server.js
+++ server.js
@@ -13,19 +13,8 @@ const db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
-});
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
-
-
-/*
 app.use(express.static(path.join(__dirname,'/public')));
 
 db.sequelize.sync().then(() => {
   http.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 });
-
-*/
