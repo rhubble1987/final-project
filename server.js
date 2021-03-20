23708402c40f507ca189544bf37d3ba8ff4df1c6
munozjuan <munoz.juan07@hotmail.com>
2021-03-20 11:30:02 -0400

    added redux

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const serveIndex = require('serve-index');
 
 const app = express();
 
@@ -12,8 +13,19 @@ const db = require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+
+
+/*
 app.use(express.static(path.join(__dirname,'/public')));
 
 db.sequelize.sync().then(() => {
   http.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 });
+
+*/
