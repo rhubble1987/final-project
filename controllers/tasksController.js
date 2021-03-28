34d3016d41d380ed6afadfe6fa5dfcf5834e78bc
rhubble1987 <69992038+rhubble1987@users.noreply.github.com>
2021-03-28 13:58:57 -0400

    Update tasksController.js

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -2,25 +2,43 @@ const db = require('../models');
 
 module.exports = {
     getUserTasks: function(req,res) {
-        db.Task.findAll({where: { Userid: req.body.userId, dueDate: '27-03-2021'}})
-        .sort({calculatedPriority: 1})
-        .then(userTasks => res.json(userTasks))
-        .catch(err => res.json(err));
+        console.log(req)
+        db.Task.findAll({where: {userId: req.body.userId, isComplete: false}})
+        .then((userTasks) => {
+            console.log(userTasks);
+            res.json(userTasks)
+        })
+        .catch(err => res.send(err));
     },
     create: function(req,res) {
         db.Task.create({
-            name: req.body.name,
-            dueData: req.body.dueDate,
-            importance: req.body.importance,
-            durationEstimate: req.body.durationEstimate,
-            calculatedWorkData: 20210331,
-            calculatedStartTime: 720,
-            calculatedEndTime: 780,
-            calculatedPriority: 1,
+            taskName: req.body.taskName,
+            dueDate: req.body.dueDate,
+            //importance: req.body.importance,
+            //durationEstimate: req.body.durationEstimate,
             note: req.body.note,
-            userId: req.body.userId
+            UserId: req.body.userId
         })
-        .then(newTask => res.json(newTask))
+        .then(function() {
+            getPriority(req.body.userId,res);
+        })
+        .catch(err => res.send(err));
+    },
+    updateUserTask: function(req,res) {
+        db.Task.update({
+            taskName: req.body.taskName,
+            dueDate: req.body.dueDate,
+            note: req.body.note,
+            UserId: req.body.userId
+        },
+        {
+            where: {
+            id: req.body.id
+            }
+        })
+        .then(function() {
+            getPriority(req.body.userId,res);
+        })
         .catch(err => res.send(err));
     }
 }
