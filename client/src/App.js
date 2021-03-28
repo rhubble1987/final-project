411118e56d580f9ece316c248e33d9a55f74d210
munozjuan <munoz.juan07@hotmail.com>
2021-03-28 14:03:33 -0400

    adding navbar that is reactive

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import Tasks from "./screens/Tasks";
-import createUser from './helpers/users.create';
+import Navbar from './components/Nav';
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>LOOPTI</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+  return (
+  <div>
+  <Router>
+  <Navbar fixed="top"/>
       <div className="outer">
         <div className="inner">
           <Switch>
@@ -34,11 +20,11 @@ function App() {
             <Route path="/sign-up" component={SignUp} />
             <Route path="/tasks" component={Tasks} />
             <Route path="/createTask" component={Tasks} />
-
           </Switch>
         </div>
       </div>
-    </div></Router>
+    </Router>
+    </div>
   );
 }
 
