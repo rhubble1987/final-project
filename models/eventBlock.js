34c97e32e0642c4193d995e54e8ae3a990151ed0
munozjuan <munoz.juan07@hotmail.com>
2021-04-03 12:41:51 -0400

    gradient / eventBlock

--- models/eventBlock.js
+++ models/eventBlock.js
@@ -1,22 +1,22 @@
 module.exports = function(sequelize, DataTypes){
-    const EventBlock = sequelize.define("EventBlock", {
-        date: { //Stored as YYYYMMDD
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        startTime: { //Stored as minutes
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        endTime: { //Stored as minutes
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        UserId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    });
+  const EventBlock = sequelize.define("EventBlock", {
+    date: { //Stored as YYYYMMDD
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    startTime: { //Stored as minutes
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    endTime: { //Stored as minutes
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
 
-    return EventBlock;
-}
+  return EventBlock;
+};
